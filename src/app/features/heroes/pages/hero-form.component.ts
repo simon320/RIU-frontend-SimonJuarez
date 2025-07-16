6cc270e8cd3d6499a4c1a5dd3c2548f764483ac3
Simon320 <simonjuarezk2@gmail.com>
2025-07-15 23:15:50 -0300

    feat: the basis for the list and form was created

---
+++ src/app/features/heroes/pages/hero-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-form',
+  imports: [],
+  templateUrl: './hero-form.component.html',
+  styleUrl: './hero-form.component.scss'
+})
+export class HeroFormComponent {
+
+}
---
+++ src/app/features/heroes/pages/hero-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-list',
+  imports: [],
+  templateUrl: './hero-list.component.html',
+  styleUrl: './hero-list.component.scss'
+})
+export class HeroListComponent {
+
+}
